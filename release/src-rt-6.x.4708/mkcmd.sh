23657cd38a21a4614ff6bf96afd64a839ef8bca7
wangmice <wangmice@126.com>
2016-09-10 08:34:36 +0800

    add mkcmd.sh
    Former-commit-id: 3fcafa64a48401b72167e58b1d323f95a27c92d0

---
+++ release/src-rt-6.x.4708/mkcmd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+case $1 in
+	1|2|3|4)
+		make ea6700z -j$1 2>&1 |tee $(date +'%Y%m%d-%H').log
+		;;
+	p)
+		make $2 2>$1 | tee $(date +'%Y%m%d-%H').log
+		;;
+	*)
+		make  ea6700z 2>&1 |tee $(date +'%Y%m%d-%H').log
+		;;
+esac
