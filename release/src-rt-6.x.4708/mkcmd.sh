e08fc7fc37cadfcdd0b1d5311935c94b8b1c0b93
wangmice <wangmice@126.com>
2016-09-10 20:33:43 +0800

    small fix
    Former-commit-id: 012ab7b59ba9786eb29baf12dfe9ab5b174fe7f2

--- release/src-rt-6.x.4708/mkcmd.sh
+++ release/src-rt-6.x.4708/mkcmd.sh
@@ -5,7 +5,7 @@ case $1 in
 		make ea6700z -j$1 2>&1 |tee $(date +'%Y%m%d-%H').log
 		;;
 	p)
-		make $2 2>$1 | tee $(date +'%Y%m%d-%H').log
+		make $2 2>&1 | tee $(date +'%Y%m%d-%H').log
 		;;
 	*)
 		make  ea6700z 2>&1 |tee $(date +'%Y%m%d-%H').log
